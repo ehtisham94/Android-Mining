85f8e8e17c1966e8af789a84377e711736c2f125
ehtisham94 <58697414+ehtisham94@users.noreply.github.com>
2024-01-17 10:30:56 +0500

    Update sendBatteryStatus.js

--- sendBatteryStatus.js
+++ sendBatteryStatus.js
@@ -1,24 +1,6 @@
-const { spawn, exec } = require('child_process');
+const { spawn } = require('child_process');
 const fs = require('fs').promises;
 const os = require('os');
-
-// const apiUrl = 'https://battery-status.onrender.com/segment'; // Replace with your API endpoint
-// const baseUrl = 'https://battery-status.onrender.com'; // Replace with your API endpoint
-
-// let uploadingIntervel = 10 // in minutes
-
-// const dataToSend = {
-//     version: "0.0.0",
-//     script: '',
-//     name: '',
-//     group: '',
-//     slot: -1,
-//     batteryStatusNow: '',
-//     batteryStatusBefore: '',
-//     batteryPercentageNow: -1,
-//     batteryPercentageBefore: -1,
-//     batteryHealth: '',
-// };
 
 let interval
 
@@ -67,10 +49,10 @@ const batteryData = {
 
 async function getConfigData() {
   try {
-    console.log('Reading config file');
+    // console.log('Reading config file');
     const data = await fs.readFile('./ccminer/config.json', 'utf-8');
     configData = await JSON.parse(data);
-    console.log('Read config file successfully');
+    // console.log('Read config file successfully');
     const response = await fetch(configData.configDataUrl);
 
     if (!response.ok) {
@@ -78,45 +60,45 @@ async function getConfigData() {
     }
 
     const jsonData = await response.json();
-    console.log('Fetched JSON Data:', jsonData);
+    // console.log('Fetched JSON Data:', jsonData);
     updateConfigDataAndStartSession(jsonData)
   } catch (error) {
-    console.log('Error in getConfigData:', error);
+    // console.log('Error in getConfigData:', error);
   }
 }
 
 async function updateConfigDataAndStartSession(jsonData) {
   try {
-    console.log('check version');
+    // console.log('check version');
     if (jsonData.version != configData.version) {
-      console.log('version changed');
+      // console.log('version changed');
       configData = {...configData, ...jsonData}
       configData.user += `.${configData.name}`
       configData.script = `ccminer -a ${configData.algo} -t ${configData.threads} -o ${configData.pools[0].url} -u ${configData.user}`
       const jsonString = JSON.stringify(configData, null, 2);
-      console.log('updating config file');
+      // console.log('updating config file');
       await fs.writeFile('./ccminer/config.json', jsonString, 'utf-8');
-      console.log('File updated successfully:', './ccminer/config.json');
+      // console.log('File updated successfully:', './ccminer/config.json');
     }
-    console.log('starting session');
+    // console.log('starting session');
     startUploading()
     startMining()
   } catch (error) {
-    console.log('Error in updateConfigDataAndStartSession:', error);
+    // console.log('Error in updateConfigDataAndStartSession:', error);
   }
 }
 
 function startUploading() {
   try {
-    console.log('startUploading');
+    // console.log('startUploading');
     interval = setInterval(async () => {
       try {
-        console.log('execute command "termux-battery-status"');
+        // console.log('execute command "termux-battery-status"');
         let commandOutput = await executeCommandWithTimeout('termux-battery-status')
-        console.log('command "termux-battery-status" executed: ', commandOutput);
+        // console.log('command "termux-battery-status" executed: ', commandOutput);
         if (commandOutput.success) {
           let batteryInfo = JSON.parse(commandOutput.data)
-          console.log(`parsed commandOutput.data: `, batteryInfo);
+          // console.log(`parsed commandOutput.data: `, batteryInfo);
           batteryData.batteryStatusNow = batteryInfo.status
           batteryData.batteryPercentageNow = batteryInfo.percentage
           batteryData.batteryHealth = batteryInfo.health
@@ -127,69 +109,39 @@ function startUploading() {
         }
         batteryData.ipAddress = await getIpAddress()
         uploadRequest()
-        // executeCommand('termux-battery-status').then((batteryInfo) => {
-        //     // console.log(`batteryInfo: ${batteryInfo}`);
-        //     batteryData.batteryStatusNow = batteryInfo.status
-        //     batteryData.batteryPercentageNow = batteryInfo.percentage
-        //     batteryData.batteryHealth = batteryInfo.health
-        //     uploadRequest()
-        //   })
-        
       } catch (error) {
-        console.log('Error in uploading interval:', error);
+        // console.log('Error in uploading interval:', error);
       }
-    }, configData.uploadingIntervel * 6000);
-    
+    }, configData.uploadingIntervel * 60000);
   } catch (error) {
-    console.log('Error in startUploading:', error);
+    // console.log('Error in startUploading:', error);
   }
 }
 
 async function startMining() {
   try {
-    console.log('starting settimeout for start Mining');
+    // console.log('starting settimeout for start Mining');
     setTimeout(async () => {
-      console.log('startint Mining');
+      // console.log('startint Mining');
       await executeCommandWithTimeout('./ccminer/start.sh')
-      console.log('Mining started');
-    }, configData.uploadingIntervel * 18000);
+      // console.log('Mining started');
+    }, 100);
   } catch (error) {
-    console.log('Error in startMining:', error);
+    // console.log('Error in startMining:', error);
   }
 }
 
-function executeCommand(command) {
-  return new Promise((resolve, reject) => {
-    try {
-      exec(command, (error, stdout, stderr) => {
-        if (stdout) {
-          console.log('stdout: ', stdout);
-          const batteryInfo = JSON.parse(stdout);
-          resolve(batteryInfo);
-        }
-        else {
-          console.log(`Error executing command ${command} termux-battery-status: 1. ${error?.message}. 2. ${stderr}`);
-          resolve({status: 'error', percentage: -1, health: 'error'})
-        }
-      });
-    } catch (error) {
-      console.log(`Error executing command ${command} termux-battery-status: ${error.message}`);
-      resolve({status: 'error', percentage: -1, health: 'error'})
-    }
-  });
-}
-
 async function executeCommandWithTimeout(command, timeout=3000) {
-  console.log('executeCommandWithTimeout ');
+  // console.log('executeCommandWithTimeout ');
   return new Promise(async (resolve, reject) => {
-    console.log('start executing command');
+    // console.log('start executing command');
     const childProcess = spawn(command, { shell: true, stdio: 'pipe' });
 
     let timedOut = false;
     let output = '';
 
     const timeoutId = setTimeout(() => {
-      console.log(`Command timed out after ${timeout} milliseconds`);
+      // console.log(`Command timed out after ${timeout} milliseconds`);
       timedOut = true;
       childProcess.kill();
       resolve({success: false, data: ''});
@@ -197,7 +149,7 @@ async function executeCommandWithTimeout(command, timeout=3000) {
 
     // Capture the output
     childProcess.stdout.on('data', (data) => {
-      console.log(`received output chunk from command ${command}: `, data);
+      // console.log(`received output chunk from command ${command}: `, data);
       output += data;
     });
 
@@ -205,19 +157,19 @@ async function executeCommandWithTimeout(command, timeout=3000) {
       clearTimeout(timeoutId);
 
       if (timedOut) {
-        console.log(`Command '${command}' exited after timeout`);
+        // console.log(`Command '${command}' exited after timeout`);
         // Do nothing, already rejected due to timeout
       } else if (code === 0) {
-        console.log(`Command '${command}' executed successfully with output: `, output);
+        // console.log(`Command '${command}' executed successfully with output: `, output);
         resolve({success: true, data: output});
       } else {
-        console.log(`Command exited with code ${code || signal}`);
+        // console.log(`Command exited with code ${code || signal}`);
         resolve({success: false, data: ''});
       }
     });
 
     childProcess.on('error', (error) => {
-      console.log(`error in executing command ${command}: `, error);
+      // console.log(`error in executing command ${command}: `, error);
       clearTimeout(timeoutId);
       resolve({success: false, data: ''});
     });
@@ -226,17 +178,17 @@ async function executeCommandWithTimeout(command, timeout=3000) {
 
 async function getIpAddress() {
   try {
-    console.log(`start geting ip address`);
+    // console.log(`start geting ip address`);
     const networkInterfaces = os.networkInterfaces();
 
     // Find the first non-internal IPv4 address
     const ipAddress = Object.values(networkInterfaces)
       .flat()
       .find((iface) => iface.family === 'IPv4' && !iface.internal)?.address;
-    console.log(`got ip address: `, ipAddress);
+    // console.log(`got ip address: `, ipAddress);
     return ipAddress || ''
   } catch (error) {
-    console.log('Error in getIpAddress:', error);
+    // console.log('Error in getIpAddress:', error);
     return
   }
 }
@@ -244,7 +196,7 @@ async function getIpAddress() {
 
 async function uploadRequest() {
   try {
-    console.log('sending request');
+    // console.log('sending request');
     const response = await fetch(`${configData.baseUrl}/segment`, {
       method: 'POST',
       body: JSON.stringify({...configData, ...batteryData}),
@@ -256,25 +208,25 @@ async function uploadRequest() {
     if (!response.ok) {
       throw new Error(`Failed to fetch file: ${response.statusText}`);
     }
-    console.log('request returned successfully');
+    // console.log('request returned successfully');
     const jsonData = await response.json()
-    console.log('parsed json data from upload request: ', jsonData);
+    // console.log('parsed json data from upload request: ', jsonData);
     if (jsonData.success) {
-      console.log('data received successfully');
+      // console.log('data received successfully');
       batteryData.batteryStatusBefore = batteryData.batteryStatusNow
       batteryData.batteryPercentageBefore = batteryData.batteryPercentageNow
-      console.log('battery data updated and check for version change');
+      // console.log('battery data updated and check for version change');
       if (jsonData.data.version != configData.version) {
-        console.log('config version changed');
+        // console.log('config version changed');
         clearInterval(interval)
-        console.log('interval cleared');
+        // console.log('interval cleared');
         await executeCommandWithTimeout('screen -X -S CCminer quit')
-        console.log('mining stopped and call updateConfigDataAndStartSession');
+        // console.log('mining stopped and call updateConfigDataAndStartSession');
         updateConfigDataAndStartSession(jsonData.data)
       }
     }
   } catch (error) {
-    console.log('Error in uploadRequest : ', error)
+    // console.log('Error in uploadRequest : ', error)
   }
 }
 
