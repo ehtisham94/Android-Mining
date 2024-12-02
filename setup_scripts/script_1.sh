2876e852f876a741a2253707d4d21923d3d995ec
ehtisham94 <58697414+ehtisham94@users.noreply.github.com>
2024-12-02 16:42:30 +0500

    Update script_1.sh

--- setup_scripts/script_1.sh
+++ setup_scripts/script_1.sh
@@ -14,4 +14,32 @@ echo "Key1: $key1"
 echo "Key2: $key2"
 echo "Key3: $key3"
 
+# Input variable
+# name="s8_001"
+
+# Extract the model key from the `name` variable (assuming the pattern before "_" is the key)
+model_key=$(echo "$name" | cut -d'_' -f1)
+
+# Declare a mapping
+declare -A model_map=(
+    ["stylo5"]="a53"
+    ["s8"]="a73-a53"
+    ["v30"]="a73-a53"
+    ["v30+"]="a73-a53"
+    ["v35"]="a75-a55"
+    ["v40"]="a75-a55"
+    ["v50"]="a76-a55"
+    ["g8"]="a76-a55"
+    ["g8x"]="a76-a55"
+    ["velvet"]="a76-a55"
+)
+
+# Get the mapped value
+mapped_value=${model_map[$model_key]}
+
+# Print or use the new variable
+echo "name: $name"
+echo "Model Key: $model_key"
+echo "Mapped Value: $mapped_value"
+
 echo "end"
